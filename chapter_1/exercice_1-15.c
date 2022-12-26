ccad1959f7810f59388db9245ec368fc384619e7
Alann <alannmaricourt15@gmail.com>
2022-12-26 19:51:50 +0100

    exercice 1-15

---
+++ chapter_1/exercice_1-15.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300; floating point version */
+
+void calcul (float fahr, float celsius, int upper, int step);
+
+int main ()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;	// lower limit of temperature table
+	upper = 300;	// upper limit
+	step = 20;	//step size
+	
+	printf("Convertion table\nFarenheit-Celsius\n");
+
+	fahr = lower;
+	calcul(fahr,celsius,upper,step);
+}
+
+void calcul (float fahr, float celsius, int upper,int step)
+{
+	while (fahr <= upper)
+	{
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f \t %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+
+}
